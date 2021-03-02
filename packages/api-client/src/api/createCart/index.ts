d841497ddf63c2bf901b334108676370a61b7c28
Aureatelabs Team <hello@aureatelabs.com>
2021-03-02 15:59:48 +0530

    Removed cookie dependency from api-client

--- packages/api-client/src/api/createCart/index.ts
+++ packages/api-client/src/api/createCart/index.ts
@@ -1,16 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { CustomQuery } from '@vue-storefront/core';
-import Cookie from 'js-cookie';
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default async function createCart(context, _params, _customQuery?: CustomQuery) {
-  // return checkout id if already exists
-  if (Cookie.get('CurCart')) {
-    return Cookie.get('CurCart');
-  }
   // initiate the cart
   return await context.client.checkout.create().then((checkout) => {
     // return checkOut ID
-    Cookie.set('CurCart', checkout.id);
     return checkout.id;
   });
 }
