2029670c4d8f9f261a792d9361098457a1cae74e
jason.wolf <jaydubb12@gmail.com>
2021-05-27 10:27:40 -0400

    Initial commit

---
+++ jw-vuestorefront/components/UserShippingAddress.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class='shipping-addr' :address="address">
+    <section>
+      <p><strong>{{ address.name }}</strong></p>
+      <p><i>{{ address.company }}</i></p>
+      <p><i>{{ address.formattedArea }}</i></p>
+      <p><i>{{ address.phone }}</i></p>
+      <p><i>{{ address.email }}</i></p>
+    </section>
+  </div>
+</template>
+
+<script type="module">
+export default {
+  name: 'UserShippingAddress',
+  props: {
+    address: {
+      default: {},
+      type: Object
+    }
+  }
+};
+</script>
