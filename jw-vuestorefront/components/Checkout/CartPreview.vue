611314372ab2ba576635ef7d8e6ccb41bc48bb96
jason.wolf <jaydubb12@gmail.com>
2021-06-03 13:24:00 -0400

    Commit of cleaned up code base

--- jw-vuestorefront/components/Checkout/CartPreview.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <b>Please implement vendor specific CartPreview component in 'components/Checkout' directory</b>
-</template>
-
-<script>
-export default {
-  name: 'CartPreview'
-};
-</script>
