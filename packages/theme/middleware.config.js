a1923d7841f26bec41f7517b4c40c2b19670e226
Aureatelabs Team <hello@aureatelabs.com>
2021-11-19 15:02:57 +0530

    fix(removed creds from middleware): removed store creds from middleware

--- packages/theme/middleware.config.js
+++ packages/theme/middleware.config.js
@@ -4,8 +4,8 @@ module.exports = {
       location: '@vue-storefront/shopify-api/server',
       configuration: {
         api: {
-          domain: 'trendhooper.myshopify.com',
-          storefrontAccessToken: 'f652044ca76e751170c1d9211c9a565c'
+          domain: process.env.SHOPIFY_DOMAIN,
+          storefrontAccessToken: process.env.SHOPIFY_STOREFRONT_TOKEN
         },
         currency: 'USD',
         country: 'US'
