b5c429521061309ba28cc94e70592ca8287a92f9
Jeff Paltera <jpaltera@vuestorefront.io>
2022-02-14 18:04:03 +0800

    chore: update getPage unit testing

--- packages/apollo/__tests__/getPage.spec.ts
+++ packages/apollo/__tests__/getPage.spec.ts
@@ -1,14 +1,53 @@
-import { contextMock } from '../__mocks__/contextMock.spec'
 import getPage from '../src/api/getPage'
 
-contextMock.client.apolloClient.query.mockImplementation((payload) => Promise.resolve(payload))
+const queryMock = jest.fn()
 
-describe('[shopify-apollo] get page', () => {
-  it('id, handle props correctly mapped', async () => {
-    const params = { handle: 'who-we-are', id: 'id' }
+const mockClient = { query: queryMock }
 
-    const mockResponse: { variables: string } = await getPage(contextMock, params) as any
+const mockExtendQuery = jest.fn();
 
-    expect(mockResponse.variables).toEqual(params)
+const mockContext: any = {
+  extendQuery: mockExtendQuery,
+  client: {
+    apolloClient: mockClient
+  }
+}
+
+describe('[shopify-apollo] mapping of params into graphql client', () => {
+  it('should map params to the page query', async () => {
+    const params = {
+      handle: 'test-handle',
+      id: 'test-id'
+    }
+
+    const customQuery = {
+      customQuery: 'customQuery-test'
+    }
+
+    mockExtendQuery.mockImplementationOnce(() => ({ page: { query: 'test-page-query', variables: params } }))
+
+    await getPage(mockContext, params, customQuery)
+
+    expect(mockExtendQuery).toHaveBeenCalledWith(customQuery, {
+      page: {
+        query: expect.any(Object),
+        variables: params
+      }
+    })
+  })
+
+  it('should execute the query with mapped params', async () => {
+    const params = {
+      handle: 'test-handle',
+      id: 'test-id'
+    }
+
+    const expectedQueryOptions = { query: 'test-page-query', variables: params }
+
+    mockExtendQuery.mockImplementationOnce(() => ({ page: expectedQueryOptions }));
+
+    await getPage(mockContext, params)
+
+    expect(queryMock).toHaveBeenCalledWith(expectedQueryOptions)
   })
 })
