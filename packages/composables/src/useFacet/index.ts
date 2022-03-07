cf07b5a1c8d56b8de58dff7bd1c503b0260b8ee9
Jeff Paltera <jeffpaltera@gmail.com>
2022-03-07 14:36:08 +0800

    fix: disabled add to cart in category (#228)
    * chore: update getPage to use apollo
    
    * chore: pr reviews resolve
    
    * chore: update getCategory
    
    * chore: update getCategory
    
    * fix: disabled cart to button when inventory is zero
    
    Co-authored-by: Jeff Paltera <jpaltera@vuestorefront.io>

--- packages/composables/src/useFacet/index.ts
+++ packages/composables/src/useFacet/index.ts
@@ -7,47 +7,32 @@ import { enhanceProduct } from './../helpers/internals';
 // TODO: move to the config file
 const ITEMS_PER_PAGE = [20, 40, 100];
 
-type LocalCollection =  Omit<Collection, 'products'> & {
-  products: LocalProduct[]  | ProductConnection
+type LocalCollection = Omit<Collection, 'products'> & {
+  products: LocalProduct[] | ProductConnection
 }
 
 const factoryParams = {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   search: async (context: Context | NewContext, params: FacetSearchResult<any>) => {
-    if (Object.keys(params.input.filters).length > 0) {
-      const newContext = context as NewContext
+    // if (Object.keys(params.input.filters).length > 0) {
+    const newContext = context as NewContext
 
-      const collection = await newContext.$shopify.api.getCollection(params.input)
+    const collection = await newContext.$shopify.api.getCollection(params.input)
 
-      const result: LocalCollection = collection.data.collection
+    const result: LocalCollection = collection.data.collection
 
-      result.products = convertProductsGqlToLocal(result.products as ProductConnection)
+    result.products = convertProductsGqlToLocal(result.products as ProductConnection)
 
-      const itemsPerPage = params.input.itemsPerPage;
+    const itemsPerPage = params.input.itemsPerPage;
 
-      return {
-        products: result.products,
-        categories: result,
-        total: result.products.length,
-        perPageOptions: ITEMS_PER_PAGE,
-        itemsPerPage
-      };
-      
-    } else {
-      const itemsPerPage = params.input.itemsPerPage;
-      const categorySlug = params.input.categorySlug;
-      const categories = await context.$shopify.api.getCategory({ slug: categorySlug });
+    return {
+      products: enhanceProduct(result.products),
+      categories: result,
+      total: result.products.length,
+      perPageOptions: ITEMS_PER_PAGE,
+      itemsPerPage
+    };
 
-      const products = enhanceProduct(categories.products);
-      return {
-        products,
-        categories,
-        total: categories.products.length,
-        perPageOptions: ITEMS_PER_PAGE,
-        itemsPerPage
-      };
-    }
-   
+
   }
 };
 
