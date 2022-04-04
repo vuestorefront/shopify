37da8192f1146246fedda8254fe98281dd8b4576
Jeff Paltera <jpaltera@vuestorefront.io>
2022-04-04 16:16:48 +0800

    fix(deploy): missing optional chain

--- packages/composables/src/helpers/internals/enhanceProduct.ts
+++ packages/composables/src/helpers/internals/enhanceProduct.ts
@@ -15,20 +15,20 @@ const enhanceProduct = (productResponse) => {
     name: product.title,
     images: product?.images,
     price: {
-      original: product.variantBySelectedOptions && product.variantBySelectedOptions !== null ? product.variantBySelectedOptions?.compareAtPriceV2?.amount : product.variants ? product.variants[0].compareAtPriceV2?.amount : 0,
-      current: product.variantBySelectedOptions && product.variantBySelectedOptions !== null ? product.variantBySelectedOptions?.priceV2?.amount : product.variants ? product.variants[0].priceV2?.amount : 0
+      original: product.variantBySelectedOptions && product.variantBySelectedOptions !== null ? product.variantBySelectedOptions?.compareAtPriceV2?.amount : product.variants ? product.variants?.[0].compareAtPriceV2?.amount : 0,
+      current: product.variantBySelectedOptions && product.variantBySelectedOptions !== null ? product.variantBySelectedOptions?.priceV2?.amount : product.variants ? product.variants?.[0].priceV2?.amount : 0
     },
-    available: product.variantBySelectedOptions && product.variantBySelectedOptions !== null ? product.variantBySelectedOptions?.quantityAvailable : product.variants ? product.variants[0].availableForSale : true,
+    available: product.variantBySelectedOptions && product.variantBySelectedOptions !== null ? product.variantBySelectedOptions?.quantityAvailable : product.variants ? product.variants?.[0].availableForSale : true,
     productType: product.productType,
     options: product.options,
     _id: product.id,
-    variantId: product.variants ? product.variants[0].id : '',
+    variantId: product.variants ? product.variants?.[0].id : '',
     _description: product.description,
     _descriptionHtml: product.descriptionHtml,
     _categoriesRef: [],
     _slug: product.handle,
-    _coverImage: product?.images[0],
-    _mainPrice: product.variants ? product.variants[0].priceV2?.amount : 0
+    _coverImage: product?.images?.[0] ?? '',
+    _mainPrice: product.variants ? product.variants?.[0].priceV2?.amount : 0
   }) as EnhancedProduct);
   return enhancedProductResponse;
 };
